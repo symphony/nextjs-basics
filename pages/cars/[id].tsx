5686b759b4793cd9c01ac26fe2382ecda52dd03d
symphony <jciurc@gmail.com>
2022-07-07 15:53:06 -0600

    set up props and data fetch for cars route

--- pages/cars/[id].tsx
+++ pages/cars/[id].tsx
@@ -1,8 +1,35 @@
 import { useRouter } from 'next/router';
 
-export default function Car() {
+import Head from 'next/head';
+
+export default function Car({ car }) {
   const router = useRouter()
   const { id } = router.query;
 
-  return <h1>car {id}</h1>
+  return (<>
+    <Head><title>{car.color} {car.id}</title></Head>
+    <h1>car {id}</h1>
+    <img src={car.image} />
+  </>)
+};
+
+export async function getStaticProps({ params }) {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  };
+};
+
+export async function getStaticPaths() {
+  const req = await fetch('http://localhost:3000/cars.json');
+  const data = await req.json();
+
+  const paths = data.map((car) => ({ params: { id: car } }))
+
+  return {
+    paths,
+    fallback: false
+  };
 };
